8eb8eeacdb9812d9d835f7e5d2f2b107769c2087
kilamieaz <im.sultaan05@gmail.com>
2025-07-09 13:35:50 +0700

    feat: add example page and login form components with authentication logic

---
+++ app/pages/example/test.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+definePageMeta({
+  title: 'Test',
+  middleware: ['auth-client'],
+  layout: 'auth',
+})
+
+const logout = useLogout()
+
+function handleLogout() {
+  logout.mutate()
+}
+</script>
+
+<template>
+  <div class="text-center py-20">
+    <h1 class="text-3xl font-semibold mb-8">
+      Example Page
+    </h1>
+
+    <p class="mb-6">
+      This is an example page to demonstrate the structure of a Vue component.
+    </p>
+
+    <CoreButton
+      label="Logout"
+      icon="i-mdi-logout"
+      class="mt-4"
+      severity="danger"
+      @click="handleLogout"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ modules/auth/components/AuthLoginForm.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { LoginInput } from '~/types/auth/login'
+import { toTypedSchema } from '@vee-validate/zod'
+import { LoginSchema } from '~/types/auth/login'
+
+const schema = toTypedSchema(LoginSchema)
+const submitError = ref('')
+const { mutateAsync, isPending } = useLogin()
+
+async function onSubmit(values: LoginInput) {
+  try {
+    const { data } = await mutateAsync(values)
+    navigateTo('/example/test')
+  }
+  catch (err: any) {
+    submitError.value = err.message ?? 'Login gagal'
+  }
+}
+</script>
+
+<template>
+  <VeeForm :validation-schema="schema" class="space-y-4" @submit="onSubmit">
+    <VeeField v-slot="{ field, errorMessage }" name="email">
+      <CoreInputText v-bind="field" placeholder="Email" />
+      <small class="text-red-500">{{ errorMessage }}</small>
+    </VeeField>
+    <VeeField v-slot="{ field, errorMessage }" name="password">
+      <CoreInputText type="password" v-bind="field" placeholder="Password" />
+      <small class="text-red-500">{{ errorMessage }}</small>
+    </VeeField>
+
+    <CoreButton type="submit" :label="isPending ? 'Logging in...' : 'Login'" />
+
+    <small v-if="submitError" class="text-red-500 block">{{ submitError }}</small>
+  </VeeForm>
+</template>
